425aa3bcb17b677932b9c3708a035ec218e996a0
divz2711 <divyasampathc@gmail.com>
2024-07-11 23:15:53 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// Example of how you might add JavaScript for interactivity
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('JavaScript loaded!');
+    // Add interactivity here
+});
